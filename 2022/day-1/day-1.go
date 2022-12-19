71f5a52fe176294641ab82399470333cc70acca8
scott <18712626+scott789@users.noreply.github.com>
2022-12-19 11:46:40 -0500

    Complete day-1 part 1

--- 2022/day-1/day-1.go
+++ 2022/day-1/day-1.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "os"
+	"strconv"
+)
 
 func main() {
-    fmt.Println("hello world")
+	readFile, err := os.Open("day-1-input.txt")
+  
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    fileScanner := bufio.NewScanner(readFile)
+ 
+    fileScanner.Split(bufio.ScanLines)
+
+	var runningTotal, maximum int
+  
+    for fileScanner.Scan() {
+		line := fileScanner.Text()
+		if len(line) != 0 {
+			i, err := strconv.Atoi(line)
+   		 	if err != nil {
+        		panic(err)
+			}
+			runningTotal += i
+    	} else {
+			if runningTotal > maximum {
+				maximum = runningTotal
+			}
+			runningTotal = 0
+		}
+    }
+	readFile.Close()
+	fmt.Println(maximum)
 }
